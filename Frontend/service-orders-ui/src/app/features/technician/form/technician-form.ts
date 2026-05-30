c35e60c38e5645e0e0c1183b84016321619b20ee
schisavo <sebastianchisavoforero@gmail.com>
2026-05-30 16:51:12 -0500

    fix: Clean architecture

--- Frontend/service-orders-ui/src/app/features/technician/form/technician-form.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-technician-form',
-  imports: [],
-  templateUrl: './technician-form.html',
-  styleUrl: './technician-form.css',
-})
-export class TechnicianForm {}
